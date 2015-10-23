7c8d4a0fac75a54f861d7fd49ad565005221b3e2
Dan Garner <dan@springsignage.com>
2015-10-23 13:23:56 +0100

    Added Change Mode

--- src/XMR/ChangeLayoutAction.php
+++ src/XMR/ChangeLayoutAction.php
@@ -14,19 +14,22 @@ class ChangeLayoutAction extends PlayerAction
     public $layoutId;
     public $duration;
     public $downloadRequired;
+    public $changeMode;
 
     /**
      * Set details for this layout
      * @param int $layoutId the layoutId to change to
      * @param int $duration the duration this layout should be shown
      * @param bool|false $downloadRequired flag indicating whether a download is required before changing to the layout
+     * @param string $changeMode whether to queue or replace
      * @return $this
      */
-    public function setLayoutDetails($layoutId, $duration = 0, $downloadRequired = false)
+    public function setLayoutDetails($layoutId, $duration = 0, $downloadRequired = false, $changeMode = 'queue')
     {
         $this->layoutId = $layoutId;
         $this->duration = $duration;
         $this->downloadRequired = $downloadRequired;
+        $this->changeMode = $changeMode;
 
         return $this;
     }
@@ -38,6 +41,6 @@ class ChangeLayoutAction extends PlayerAction
         if ($this->layoutId == 0)
             throw new PlayerActionException('Layout Details not provided');
 
-        return $this->serializeToJson(['layoutId', 'duration', 'downloadRequired']);
+        return $this->serializeToJson(['layoutId', 'duration', 'downloadRequired', 'changeMode']);
     }
 }
