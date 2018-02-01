1a2f92fb037005487dd1b06e4fc35cd9098a7b36
Alex Harrington <alex@springsignage.com>
2018-02-01 14:16:55 +0000

    Make XMR bind to all interfaces, so that it's suitable for use in Kubernetes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Get our running IP address
-ip=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
-
 # Write config.json
 echo '{' > /opt/xmr/config.json
-echo '  "listenOn": "tcp://'$ip':50001",' >> /opt/xmr/config.json
-echo '  "pubOn": ["tcp://'$ip':9505"],' >> /opt/xmr/config.json
+echo '  "listenOn": "tcp://*:50001",' >> /opt/xmr/config.json
+echo '  "pubOn": ["tcp://*:9505"],' >> /opt/xmr/config.json
 echo '  "queuePoll": '$XMR_QUEUE_POLL',' >> /opt/xmr/config.json
 echo '  "queueSize": '$XMR_QUEUE_SIZE',' >> /opt/xmr/config.json
 echo '  "debug": '$XMR_DEBUG',' >> /opt/xmr/config.json
